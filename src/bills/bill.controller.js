d043ae9348c80bba9c6bdd368f1b60e64d8984d8
IanAlfaroSantos <ialfasa2020@gmail.com>
2025-03-04 20:30:15 -0600

    CRUD Bill added incomplete

--- src/bills/bill.controller.js
+++ src/bills/bill.controller.js
@@ -16,8 +16,8 @@ export const generateBill = async (req, res) => {
                 msg: "You do not have permission to generate a bill for another user"
             });
         }
-
-        //verifica que el carrito exista
+        
+        //verifica que el carrito exista en la base de datos
         if (!cart) {
             return res.status(400).json({
                 success: false,
@@ -42,36 +42,86 @@ export const generateBill = async (req, res) => {
                 amount: cart.products[i].amount
             });
         }
-  
+        
         const bill = new Bill({
             user: id,
             products: products,
             total: total
         });
-
+        
         await bill.save();
 
         cart.products = [];
         await cart.save();
-
+        
         const detailsBill = await Bill.findById(bill._id)
-            .populate('user', 'username')
-            .populate({
-                path: 'products.product',
-                select: 'nameProduct price'
-            });
-  
+        .populate('user', 'username')
+        .populate({
+            path: 'products.product',
+            select: 'nameProduct price'
+        });
+        
         res.status(200).json({
             success: true,
             msg: "Bill generated successfully",
             detailsBill
         });
-  
+        
     } catch (error) {
-        console.error(error);
         res.status(500).json({
             success: false, msg:
             "Error generating bill",
+            error
+        });
+    }
+};
+
+export const getBillUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        
+        //verifica que el usuario ingresado sea el mismo que el usuario que va a buscar sus facturas
+        const user = await User.findById(id);
+        if (req.user.id !== user._id.toString()) {
+            return res.status(400).json({
+                success: false,
+                msg: "You do not have permission to search a bill for another user"
+            });
+        }
+
+        const bills = await Bill.find({ user: id })
+        .populate('user', 'username')
+        .populate({
+            path: 'products.product',
+                select: 'nameProduct price'
+            });
+        
+        //verifica que la factura exista en la base de datos
+        if (!bills) {
+            return res.status(404).json({
+                success: false,
+                msg: "No bills found for this user"
+            });
+        }
+
+        //verifica que la lista de facturas no este vacía
+        if (bills.length === 0) {
+            return res.status(200).json({
+                success: true,
+                msg: "No bills found for this user"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            bills
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            msg: "Error retrieving bills",
             error
         });
     }
--- src/bills/bill.routes.js
+++ src/bills/bill.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
-import { validarJWT } from "../middlewares/validar-jwt.js"
+import { check } from "express-validator";
+import { validarJWT } from "../middlewares/validar-jwt.js";
+import { existeBillById } from "../helpers/db-validator.js";
+import { validarCampos } from "../middlewares/validar-campos.js";
 import { generateBill } from "./bill.controller.js";
 
 const router = Router();
@@ -8,6 +11,17 @@ router.post(
     "/:id",
     validarJWT,
     generateBill
+);
+
+router.get(
+    "/:id",
+    [
+        validarJWT,
+        check("id", "Invalid ID").not().isEmpty(),
+        check("id").custom(existeBillById),
+        validarCampos
+    ],
+    getBillById
 );
 
 export default router;
